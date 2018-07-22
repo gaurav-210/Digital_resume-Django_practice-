3882f75892007b9c8eff071152f729c77b07e35c
gaurav-210 <karan02555@gmail.com>
2018-07-22 16:41:42 +0530

    digital resume (django practice)

---
+++ resume/resume/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse       # Returns response
+from django.shortcuts import render     # To render to a template ..
+
+
+
+
+
+
+
+
+
+
+def GauravPage(request):      # Request response ..
+    return render(request, "resume.html" , {})
+
